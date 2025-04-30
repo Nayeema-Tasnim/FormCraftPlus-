62e55d06ac16017b760b9727673b40381385bb14
Naima <naima@example.com>
2025-04-30 18:24:49 +0600

    Add pending EF migration

--- obj/Debug/net9.0/finalproject.AssemblyInfo.cs
+++ obj/Debug/net9.0/finalproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("finalproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8610baf451b4f8ad2c9787a50b9dc70a6daf0e0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9a5975dc31e1a3415a1b0a3ffb7d3ec747a9281")]
 [assembly: System.Reflection.AssemblyProductAttribute("finalproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("finalproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
