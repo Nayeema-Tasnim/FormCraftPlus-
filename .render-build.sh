5fcceacee550dd3933671de3223826530091d0de
Nayeema-Tasnim <124617804+Nayeema-Tasnim@users.noreply.github.com>
2025-04-30 14:45:13 +0600

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Install stable .NET 9 SDK
+# Install .NET 9.0.100 SDK
 wget https://dot.net/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --version 9.0.100 --install-dir ./dotnet
 
-# Add to PATH
-export PATH="$PATH
+# Add .NET to PATH
+export PATH="$PATH:$(pwd)/dotnet"
+
+# Log installed version
+./dotnet/dotnet --version
+
+# Publish the project
+./dotnet/dotnet publish -c Release -o out
